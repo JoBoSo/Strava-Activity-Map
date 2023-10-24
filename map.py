ccd9e68714f340d1a995963ef46ba14acdc8cfb6
James Scott <jamesbscott@hotmail.ca>
2023-10-24 15:46:29 -0700

    styling

--- map.py
+++ map.py
@@ -8,36 +8,17 @@ my_dir = os.path.dirname(__file__)
 json_file_path = os.path.join(my_dir, 'data.json')
 df = pd.read_json(json_file_path)
 
-# zoom - can adjust the subtraction factor (10 - 12)
-# lat_num = [float(i) for i in lat]
-# lon_num = [float(i) for i in lon]
-# max_bound = max(abs(max(lat_num)-min(lat_num)), abs(max(lon_num)-min(lon_num))) * 111
-# zoom = 11.5 - np.log(max_bound)
-
 def map():
-    fig = px.density_mapbox(
+    fig = px.scatter_mapbox(
         df, 
         lat='lat', 
         lon='lon',
-        # center=dict(lat=mean(lat_num), lon=mean(lon_num)),
-        # zoom=zoom,
         center=dict(lat=48, lon=-96),
         zoom=3.3,
-        # mapbox_style="carto-positron",
         opacity = 0.3,
-        radius = 2.5,
-        range_color = [0,250000],
-        color_continuous_scale='inferno'
-    )
-
-    fig.add_trace(
-        go.Scattermapbox(
-            lat=df["lat"],
-            lon=df["lon"],
-            mode="markers",
-            showlegend=False,
-            hoverinfo="skip",
-        )
+        color_discrete_sequence=['#00ff8d'],
+        hover_name='time',
+        hover_data=['activity', 'lat', 'lon', 'elevation']
     )
 
     fig.update_layout(
